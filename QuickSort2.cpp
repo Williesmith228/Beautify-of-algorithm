e1422d0d8ab45d26e451da7a3c6d69284602df41
Williesmith228 <wuzl_09@163.com>
2016-06-15 10:29:56 +0800

    Update QuickSort2.cpp

--- QuickSort2.cpp
+++ QuickSort2.cpp
@@ -1,4 +1,30 @@
 //Basic idea about quicksort
-QuickSort(A,p,r)
-  if(p<r) then
-    
+//从两头寻找
+int Partion(int A[], int p, int r)
+{
+    if(p>r)
+        return 1;
+    int i=p,j=r;
+    int x=A[p];
+    while(i!=j)
+    {
+        while(A[j] >=x && i < j)
+        {
+            j--;
+        }
+        while(A[i] <=x && i < j)
+        {
+            i++;
+        }
+        if(i < j)
+        {
+            int temp=A[i];
+            A[i]=A[j];
+            A[j]=temp;
+        }
+    }
+    A[p]=A[i];
+    A[i]=x;
+
+    return i;
+}
